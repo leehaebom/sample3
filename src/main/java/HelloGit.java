7b47b8dce3bc4b4d6ec586cc85d263042a9478ff
leehaebom <boradola2@naver.com>
2015-05-18 11:14:51 +0900

    "HelloGit" file modify on development branch

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -5,4 +5,7 @@ public class HelloGit {
     public static void main(String args[]){
         System.out.println("Hello Git");
     }
+    public static int add(int a, int b){
+        return a+b;
+    }
 }
